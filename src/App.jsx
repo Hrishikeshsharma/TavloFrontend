d1bbd623f85b6fd1a16d2c822de07550398af358
Hrishikesh <sharmahrishikesh3@gmail.com>
2025-06-08 19:50:42 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import TopNav from "./components/TopNav";
 import RestoLogin from "./components/RestoLogin";
 import QuickLinks from "./components/QuickLinks";
 import RestoSignup from "./components/RestoSignup";
-import { useAuth, AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import BookingsList from "./components/BookingsList";
 import RestoDashboard from "./components/RestoDashboard";
 import Menu from "./components/Menu";
@@ -23,70 +23,26 @@ function App() {
   return (
     <AuthProvider>
       <div>
-        <Router>
-          {/*} <nav>
-            <Link to="/home">Home</Link>
-            <Link to="/signup">Signup</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/browse">Restaurants</Link>
-            <Link to="/restologin">Restaulogin</Link>
-            <Link to="/quicklinks"></Link>
-            <Link to="/restosignup"></Link>
-            <Link to="/userbookings">user bookings</Link>
-            <Link to="/restodashboard">RestoDashBoard</Link>
-            <Link to="/menu">Menu</Link>
-            <Link to="/pastorders">PastOrders</Link>
-            <Link to="/addresto">addresto</Link>
-            <Link to="/manageresto">Manage resto</Link>
-            <link to="/tavlo">Tavlo</Link>
-          </nav>*/}
-
-          <Routes>
-            <Route
-              path="/"
-              element={<Navigate to={"/home"}></Navigate>}
-            ></Route>
-            <Route path="/home" element={<HomePage />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/login" element={<LogIn />}></Route>
-            <Route path="/browse" element={<BrowseResto />}></Route>
-            <Route
-              path="/restologin"
-              element={<RestoLogin></RestoLogin>}
-            ></Route>
-            <Route
-              path="/quicklinks"
-              element={<QuickLinks></QuickLinks>}
-            ></Route>
-            <Route
-              path="/restosignup"
-              element={<RestoSignup></RestoSignup>}
-            ></Route>
-            <Route
-              path="/userbookings"
-              element={<BookingsList></BookingsList>}
-            ></Route>
-            <Route
-              path="/restodashboard"
-              element={<RestoDashboard></RestoDashboard>}
-            ></Route>
-            <Route path="/menu" element={<Menu></Menu>}></Route>
-            <Route path="/pastorders" element={<PastOrders />} />
-            <Route
-              path="/addresto"
-              element={<AddRestaurant></AddRestaurant>}
-            ></Route>
-            <Route
-              path="/manageresto"
-              element={<ManageResto></ManageResto>}
-            ></Route>
-            <Route path="tavlo" element={<Tavlo></Tavlo>}></Route>
-            <Route path="/book" element={<BookNow />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/home"} />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/browse" element={<BrowseResto />} />
+          <Route path="/restologin" element={<RestoLogin />} />
+          <Route path="/quicklinks" element={<QuickLinks />} />
+          <Route path="/restosignup" element={<RestoSignup />} />
+          <Route path="/userbookings" element={<BookingsList />} />
+          <Route path="/restodashboard" element={<RestoDashboard />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/pastorders" element={<PastOrders />} />
+          <Route path="/addresto" element={<AddRestaurant />} />
+          <Route path="/manageresto" element={<ManageResto />} />
+          <Route path="/tavlo" element={<Tavlo />} />
+          <Route path="/book" element={<BookNow />} />
+        </Routes>
       </div>
     </AuthProvider>
   );
 }
-
 export default App;
