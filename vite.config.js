2020530459b336c3ed5174095feec7308582ccb6
Hrishikesh <sharmahrishikesh3@gmail.com>
2025-06-08 19:34:39 +0530

    router fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "./",
   build: {
     outDir: "dist",
   },
-  base: "/", // if relative paths are needed
 });
